665dba41dbae02b2627f308c7d3a7b1c03f3a638
Kabira Singh <yogesh.baghel86@gmail.com>
2021-04-22 22:49:21 +0530

    setting up the routes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require('express');
 const app=express();
 
 const port=8000;
+
+//use express router
+app.use('/',require('./routes'));
+
 app.listen(port,(err)=>{
     if(err){
         console.log(error,err);
