29f8b03427977717daceb95639b6ea4ef3e6d599
Kabira Singh <yogesh.baghel86@gmail.com>
2021-04-24 19:04:53 +0530

    set up the signin and signup page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,23 @@ module.exports.profile=(req,res)=>{
         title:'User Profile'
     });
 }
+
+//render the signup page
+module.exports.signUp=(req,res)=>{
+    return res.render('user_sign_up',{
+        title:"MiniFacebook | Sign Up"
+    })
+}
+
+
+//render the signin page
+module.exports.signIn=(req,res)=>{
+    return res.render('user_sign_in',{
+        title:"MiniFacebook | Sign In"
+    })
+}
+
+//get the sign-up data
+module.exports.create=(req,res)=>{
+    //todo later
+}
--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const port=8000;
 const db=require('./config/mongoose');
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+// const sassMiddleware = require('node-sass-middleware');
+
+// app.use(sassMiddleware({
+//     // src:path.join(__dirname,env.asset_path,'scss'),
+//     // dest:path.join(__dirname,env.asset_path,'css'),
+//     src: './assets/scss',
+//     dest: './assets/css',
+//     debug: true,
+//     outputStyle: 'extended',
+//     prefix: '/css'
+// }));
 
 //extract style and scripts from subpages to layout
 app.set('layout extractStyles',true);
