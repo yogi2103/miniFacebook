963a7a059304f5293be6021fcccf4e1c0af6c8a9
Kabira Singh <yogesh.baghel86@gmail.com>
2021-04-26 13:43:01 +0530

    added cross relation between comment and post

--- models/post.js
+++ models/post.js
@@ -8,7 +8,12 @@ const postSchema=new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref:'User'
-    }
+    },
+    //include the array of ids of all comment schema
+    comments:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Comment'
+    }],
 },{
     timestamps:true
 });
