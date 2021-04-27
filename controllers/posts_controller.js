e15ef7f715d740a449c4080e586a0602add51e3e
Kabira Singh <yogesh.baghel86@gmail.com>
2021-04-27 12:32:35 +0530

    converting post_controller to async

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,31 +1,33 @@
 const Post=require('../models/post');
 const Comment=require('../models/comment');
-module.exports.create=(req,res)=>{
-    Post.create({                   //here we cannot use post.create(req.body) and user should also be logged in
-        content:req.body.content,
-        user:req.user._id},(err,post)=>{
-        if(err){
-            console.log(err);
-            return;
-        }
-        else{
-            return res.redirect('back');
-        }
-    })
+module.exports.create=async (req,res)=>{
+    try{
+        await Post.create({                   //here we cannot use post.create(req.body) and user should also be logged in
+            content:req.body.content,
+            user:req.user._id});
+        return res.redirect('back');
+    }
+    catch(err){
+        console.log(err);
+        return;
+    }
 }
 
 
-module.exports.destroy=(req,res)=>{
-    Post.findById(req.params.id,(err,post)=>{
-        //to check authentication who's deleting the post
-        if(post.user == req.user.id){   //.id means converting the id to string
-            post.remove();
-            Comment.deleteMany({post:req.params.id},(err)=>{
-                return res.redirect('back');
-            })
-        }
-        else{
-            return res.redirect('back');
-        }
-    })
+module.exports.destroy=async (req,res)=>{
+    try{
+        let post=await Post.findById(req.params.id);
+    if(post.user == req.user.id){   //.id means converting the id to string
+        post.remove();
+        await Comment.deleteMany({post:req.params.id});
+        return res.redirect('back');
+    }
+    else{
+        return res.redirect('bacl');
+    }
+    }
+    catch(err){
+        console.log(err);
+        return;
+    }
 }
